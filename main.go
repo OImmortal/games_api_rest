067e09282e21c0986072b938b897a408c00748f4
OImmortal <joaopedropeixotosaraiva@outlook.com>
2026-05-20 21:53:40 -0300

    Resolvendo problema do cors

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"games_api/service"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 
 func main() {
 	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://gamesapirest-production.up.railway.app"}, // Allowed origins
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},       // Allowed HTTP methods
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},       // Allowed headers
-		ExposeHeaders:    []string{"Content-Length"},                                // Headers exposed to the browser
-		AllowCredentials: true,                                                      // Allow cookies/auth headers
-		MaxAge:           12 * time.Hour,                                            // Cache preflight response
-	}))
+	r.Use(CORSMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
